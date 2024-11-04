42f009fe6df90791b63ddba9cac61ab7b6a854e1
赖笑慷 <laixiaokang@4paradigm.com>
2024-11-05 02:18:54 +0800

    up

--- finetune/train.py
+++ finetune/train.py
@@ -1,189 +1,217 @@
-import copy
-import logging
-from dataclasses import dataclass, field
-from typing import Dict, Optional, Sequence
+# -*- coding:utf-8 -*-
+# @project: ChatGPT
+# @filename: train
+# @author: 刘聪NLP
+# @zhihu: https://www.zhihu.com/people/LiuCongNLP
+# @contact: logcongcong@gmail.com
+# @time: 2023/7/24 22:45
+"""
+    文件说明：
+            
+"""
 import argparse
+import json
+import math
+from tqdm import tqdm
 import torch
-import transformers
-import utils
-from torch.utils.data import Dataset
-from transformers import Trainer
-import torch.distributed as dist
-import sys
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
+from torch.utils.data.distributed import DistributedSampler
+import deepspeed
+from transformers import BitsAndBytesConfig
+from utils import print_trainable_parameters, print_rank_0, to_device, set_random_seed, save_model, DataCollator, \
+    find_all_linear_names, evaluation
+from peft import LoraConfig, get_peft_model, prepare_model_for_kbit_training
+from qwen1_8.modeling_qwen import QWenLMHeadModel
+from qwen1_8.tokenization_qwen import QWenTokenizer
+from qwen1_8.configuration_qwen import QWenConfig
+from utils import QwenPromptDataSet
 import os
-import numpy as np
-from utils import utils
 
-IGNORE_INDEX = -1
-DEFAULT_PAD_TOKEN = "[PAD]"
-DEFAULT_EOS_TOKEN = "</s>"
-DEFAULT_BOS_TOKEN = "<s>"
-DEFAULT_UNK_TOKEN = "<unk>"
-logging.basicConfig(level=logging.DEBUG)  
-@dataclass
-class ModelArguments:
-    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
+try:
+    from torch.utils.tensorboard import SummaryWriter
+except ImportError:
+    from tensorboard import SummaryWriter
 
 
-@dataclass
-class DataArguments:
-    data_path: str = field(default=None, metadata={"help": "Path to the training data."})
+def parse_args():
+    parser = argparse.ArgumentParser()
+    # 模型配置
+    parser.add_argument("--model_name_or_path", type=str, help="", required=True)
+    # 数据配置
+    parser.add_argument("--train_path", default="", type=str, help="")
+    parser.add_argument("--test_path", default="", type=str, help="")
+    parser.add_argument("--max_len", type=int, default=1024, help="")
+    parser.add_argument("--max_src_len", type=int, default=256, help="")
+    parser.add_argument("--is_skip", action='store_true', help="")
+    # 训练配置
+    parser.add_argument("--per_device_train_batch_size", type=int, default=16, help="")
+    parser.add_argument("--learning_rate", type=float, default=1e-3, help="")
+    parser.add_argument("--weight_decay", type=float, default=0.1, help="")
+    parser.add_argument("--num_train_epochs", type=int, default=1, help="")
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1, help="")
+    parser.add_argument("--warmup_ratio", type=float, default=0.1, help="")
+    parser.add_argument("--output_dir", type=str, default=None, help="")
+    parser.add_argument("--seed", type=int, default=1234, help="")
+    parser.add_argument("--local_rank", type=int, default=-1, help="")
+    parser.add_argument("--show_loss_step", default=10, type=int, help="")
+    parser.add_argument("--gradient_checkpointing", action='store_true', help="")
+    parser.add_argument("--save_model_step", default=None, type=int, help="")
+    # DeepSpeed配置
+    parser.add_argument("--ds_file", type=str, default="ds_zero2.json", help="")
+    # QLoRA配置
+    parser.add_argument("--lora_dim", type=int, default=8, help="")
+    parser.add_argument("--lora_alpha", type=int, default=30, help="")
+    parser.add_argument("--lora_dropout", type=float, default=0.1, help="")
 
-
-@dataclass
-class TrainingArguments(transformers.TrainingArguments):
-    cache_dir: Optional[str] = field(default=None)
-    optim: str = field(default="adamw_torch")
-    model_max_length: int = field(
-        default=512,
-        metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
-    )
-    truncate_source: bool = field(default=False)
-
-
-def smart_tokenizer_and_embedding_resize(
-    special_tokens_dict: Dict,
-    tokenizer: transformers.PreTrainedTokenizer,
-    model: transformers.PreTrainedModel,
-):
-    """Resize tokenizer and embedding.
-
-    Note: This is the unoptimized version that may make your embedding size not be divisible by 64.
-    """
-    num_new_tokens = tokenizer.add_special_tokens(special_tokens_dict)
-    model.resize_token_embeddings(len(tokenizer))
-
-    if num_new_tokens > 0:
-        input_embeddings = model.get_input_embeddings().weight.data
-        output_embeddings = model.get_output_embeddings().weight.data
-
-        input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
-        output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
-
-        input_embeddings[-num_new_tokens:] = input_embeddings_avg
-        output_embeddings[-num_new_tokens:] = output_embeddings_avg
-
-
-def _tokenize_fn(strings: Sequence[str], tokenizer: transformers.PreTrainedTokenizer) -> Dict:
-    """Tokenize a list of strings."""
-    tokenized_list = [
-        tokenizer(
-            text,
-            return_tensors="pt",
-            padding="longest",
-            max_length=tokenizer.model_max_length,
-            truncation=True,
-        )
-        for text in strings
-    ]
-    input_ids = labels = [tokenized.input_ids[0] for tokenized in tokenized_list]
-    input_ids_lens = labels_lens = [
-        tokenized.input_ids.ne(tokenizer.pad_token_id).sum().item() for tokenized in tokenized_list
-    ]
-    return dict(
-        input_ids=input_ids,
-        labels=labels,
-        input_ids_lens=input_ids_lens,
-        labels_lens=labels_lens,
-    )
-
-
-class SupervisedDataset(Dataset):
-    """Dataset for supervised fine-tuning."""
-
-    def __init__(self, data_path: str, tokenizer: transformers.PreTrainedTokenizer, args):
-        super(SupervisedDataset, self).__init__()
-        logging.warning("Loading data...")
-        if data_path.endswith(".npy"):
-            list_data_dict = np.load(data_path, allow_pickle=True)
-        else:
-            list_data_dict = utils.read_jsonl_file(data_path)
-        logging.info("Loading tokenized sentences...")
-        def truncate(sentence):
-            return torch.tensor(sentence[:args.model_max_length] + [tokenizer.eos_token_id] if len(sentence) > args.model_max_length else sentence)
-        if args.truncate_source:
-            self.input_ids = [truncate(example["input_ids"]) for example in list_data_dict]
-            self.labels = [truncate(example["label"]) for example in list_data_dict]
-        else:
-            self.input_ids = [torch.tensor(example["input_ids"]) for example in list_data_dict if len(example["input_ids"]) < args.model_max_length]
-            self.labels = [torch.tensor(example["label"]) for example in list_data_dict if len(example["input_ids"]) < args.model_max_length]
-        print(f"Samples: {len(list_data_dict)} -> {len(self.input_ids)}")
-
-    def __len__(self):
-        return len(self.input_ids)
-
-    def __getitem__(self, i) -> Dict[str, torch.Tensor]:        
-        return dict(input_ids=self.input_ids[i], labels=self.labels[i])
-
-
-@dataclass
-class DataCollatorForSupervisedDataset(object):
-    """Collate examples for supervised fine-tuning."""
-
-    tokenizer: transformers.PreTrainedTokenizer
-
-    def __call__(self, instances: Sequence[Dict]) -> Dict[str, torch.Tensor]:
-        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
-        input_ids = torch.nn.utils.rnn.pad_sequence(
-            input_ids, batch_first=True, padding_value=self.tokenizer.pad_token_id
-        )
-        labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
-        return dict(
-            input_ids=input_ids,
-            labels=labels,
-            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
-        )
-
-
-def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, data_args) -> Dict:
-    """Make dataset and collator for supervised fine-tuning."""
-    train_dataset = SupervisedDataset(tokenizer=tokenizer, data_path=data_args.data_path, args=data_args)
-    data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
-    return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
-
-
-def is_master():
-    return dist.get_rank() == 0
-
-
-class LoggingCallback(transformers.TrainerCallback):
-    def on_log(self, args, state, control, logs=None, **kwargs):
-        if logs is not None:
-            log_message = {
-                "loss": logs.get("loss", None),
-                "learning_rate": logs.get("learning_rate", None),
-                "epoch": logs.get("epoch", None),
-                "step": state.global_step
-            }
-            if is_master():
-                print(log_message)
+    parser = deepspeed.add_config_arguments(parser)
+    return parser.parse_args()
 
 
 def train():
-    parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
-    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
-    args = {**model_args.__dict__, **data_args.__dict__, **training_args.__dict__}
-    args = argparse.Namespace(**args)
-    model = transformers.AutoModelForCausalLM.from_pretrained(
-        model_args.model_name_or_path,
-        cache_dir=training_args.cache_dir,
-    )
-    tokenizer = transformers.AutoTokenizer.from_pretrained(
-        model_args.model_name_or_path,
-        pad_token = '<|extra_0|>',
-        eos_token = '<|im_end|>', #<|endoftext|>
-        cache_dir = None,
-        model_max_length = training_args.model_max_length,
-        truncation = True,
-        padding_side = "right",
-        trust_remote_code = True
-    )
-    tokenizer.add_special_tokens({"additional_special_tokens": ["<|im_end|>", "<|im_start|>"]})
-    data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=args)
-    trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module, callbacks=[LoggingCallback])
-    trainer.train()
-    trainer.save_state()
-    trainer.save_model(output_dir=training_args.output_dir)
+    # 设置模型训练参数
+    args = parse_args()
+    # 判断是多卡训练还是单卡训练
+    if args.local_rank == -1:
+        device = torch.device("cuda")
+    else:
+        torch.cuda.set_device(args.local_rank)
+        device = torch.device("cuda", args.local_rank)
+        deepspeed.init_distributed()
+    args.global_rank = torch.distributed.get_rank()
+    # 设置tensorboard，记录训练过程中的loss以及ppl
+    if args.global_rank <= 0:
+        tb_write = SummaryWriter()
+    # 设置随机种子，方便模型复现
+    set_random_seed(args.seed)
+    torch.distributed.barrier()
+    # 加载千问模型分词器
+    tokenizer = QWenTokenizer.from_pretrained(args.model_name_or_path)
+    tokenizer.pad_token_id = tokenizer.eod_id
+    # 加载千问模型
+    device_map = {'': int(os.environ.get('LOCAL_RANK', '0'))}
+    model_config = QWenConfig.from_pretrained(args.model_name_or_path)
+    model = QWenLMHeadModel.from_pretrained(args.model_name_or_path,
+                                            quantization_config=BitsAndBytesConfig(
+                                                load_in_4bit=True,
+                                                bnb_4bit_compute_dtype=model_config.torch_dtype,
+                                                bnb_4bit_use_double_quant=True,
+                                                bnb_4bit_quant_type="nf4",
+                                                llm_int8_threshold=6.0,
+                                                llm_int8_has_fp16_weight=False,
+                                            ),
+                                            torch_dtype=model_config.torch_dtype,
+                                            device_map=device_map)
+    model = prepare_model_for_kbit_training(model)
+    # 找到模型中所有的全连接层
+    lora_module_name = find_all_linear_names(model)
+    # 设置Lora配置，并生成外挂可训练参数
+    config = LoraConfig(r=args.lora_dim,
+                        lora_alpha=args.lora_alpha,
+                        target_modules=lora_module_name,
+                        lora_dropout=args.lora_dropout,
+                        bias="none",
+                        task_type="CAUSAL_LM",
+                        inference_mode=False,
+                        )
+    model = get_peft_model(model, config)
+    model.config.torch_dtype = torch.float32
+    # 打印可训练参数
+    for name, param in model.named_parameters():
+        if param.requires_grad == True:
+            print_rank_0(name, 0)
+    print_trainable_parameters(model)
+
+    # 加载模型训练所需要的数据，如果是多卡训练需要分布式加载数据
+    train_dataset = QwenPromptDataSet(args.train_path, tokenizer, args.max_len, args.max_src_len, args.is_skip)
+    test_dataset = QwenPromptDataSet(args.test_path, tokenizer, args.max_len, args.max_src_len, args.is_skip)
+    if args.local_rank == -1:
+        train_sampler = RandomSampler(train_dataset)
+        test_sampler = SequentialSampler(test_dataset)
+    else:
+        train_sampler = DistributedSampler(train_dataset)
+        test_sampler = DistributedSampler(test_dataset)
+
+    data_collator = DataCollator(tokenizer)
+    train_dataloader = DataLoader(train_dataset, collate_fn=data_collator, sampler=train_sampler, batch_size=args.per_device_train_batch_size)
+    test_dataloader = DataLoader(test_dataset, collate_fn=data_collator, sampler=test_sampler, batch_size=args.per_device_train_batch_size)
+    print_rank_0("len(train_dataloader) = {}".format(len(train_dataloader)), args.global_rank)
+    print_rank_0("len(train_dataset) = {}".format(len(train_dataset)), args.global_rank)
+
+    # 加载DeepSpeed配置文件，并进行修改
+    with open(args.ds_file, "r", encoding="utf-8") as fh:
+        ds_config = json.load(fh)
+    ds_config['train_micro_batch_size_per_gpu'] = args.per_device_train_batch_size
+    ds_config[ 'train_batch_size'] = args.per_device_train_batch_size * torch.distributed.get_world_size() * args.gradient_accumulation_steps
+    ds_config['gradient_accumulation_steps'] = args.gradient_accumulation_steps
+    # load optimizer
+    ds_config["optimizer"]["params"]["lr"] = args.learning_rate
+    ds_config["optimizer"]["params"]["betas"] = (0.9, 0.95)
+    ds_config["optimizer"]["params"]["eps"] = 1e-8
+    ds_config["optimizer"]["params"]["weight_decay"] = 0.1
+    num_training_steps = args.num_train_epochs * math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
+    print_rank_0("num_training_steps = {}".format(num_training_steps), args.global_rank)
+    num_warmup_steps = int(args.warmup_ratio * num_training_steps)
+    print_rank_0("num_warmup_steps = {}".format(num_warmup_steps), args.global_rank)
+    ds_config["scheduler"]["params"]["total_num_steps"] = num_training_steps
+    ds_config["scheduler"]["params"]["warmup_num_steps"] = num_warmup_steps
+    ds_config["scheduler"]["params"]["warmup_max_lr"] = args.learning_rate
+    ds_config["scheduler"]["params"]["warmup_min_lr"] = args.learning_rate * 0.1
+
+    # 设置模型gradient_checkpointing
+    if args.gradient_checkpointing:
+        model.gradient_checkpointing_enable()
+        if hasattr(model, "enable_input_require_grads"):
+            model.enable_input_require_grads()
+        else:
+            def make_inputs_require_grad(module, input, output):
+                output.requires_grad_(True)
+
+            model.get_input_embeddings().register_forward_hook(make_inputs_require_grad)
+
+    # DeepSeed对模型进行初始化
+    model, optimizer, _, lr_scheduler = deepspeed.initialize(model=model, args=args, config=ds_config, dist_init_required=True)
+    tr_loss, logging_loss, min_loss = 0.0, 0.0, 0.0
+    global_step = 0
+    # 模型开始训练
+    for epoch in range(args.num_train_epochs):
+        print_rank_0("Beginning of Epoch {}/{}, Total Micro Batches {}".format(epoch + 1, args.num_train_epochs, len(train_dataloader)), args.global_rank)
+        model.train()
+        # 遍历所有数据
+        for step, batch in tqdm(enumerate(train_dataloader), total=len(train_dataloader), unit="batch"):
+            batch = to_device(batch, device)
+            # 获取训练结果
+            outputs = model(**batch, use_cache=False)
+            loss = outputs.loss
+            # 损失进行回传
+            model.backward(loss)
+            tr_loss += loss.item()
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            model.step()
+            # 当训练步数整除累积步数时，记录训练损失值和模型保存
+            if (step + 1) % args.gradient_accumulation_steps == 0:
+                global_step += 1
+                # 损失值记录
+                if global_step % args.show_loss_step == 0:
+                    if args.global_rank <= 0:
+                        tb_write.add_scalar("train_loss", (tr_loss - logging_loss) / (args.show_loss_step * args.gradient_accumulation_steps), global_step)
+                        logging_loss = tr_loss
+                # 模型保存并验证测试集的PPL值
+                if args.save_model_step is not None and global_step % args.save_model_step == 0:
+                    ppl = evaluation(model, test_dataloader, device)
+                    if args.global_rank <= 0:
+                        tb_write.add_scalar("ppl", ppl, global_step)
+                        print_rank_0("save_model_step-{}: ppl-{}".format(global_step, ppl), args.global_rank)
+                    if args.global_rank <= 0:
+                        save_model(model, tokenizer, args.output_dir, f"epoch-{epoch + 1}-step-{global_step}")
+                    model.train()
+        # 每个Epoch对模型进行一次测试，记录测试集的损失
+        ppl = evaluation(model, test_dataloader, device)
+        if args.global_rank <= 0:
+            tb_write.add_scalar("ppl", ppl, global_step)
+            print_rank_0("save_model_step-{}: ppl-{}".format(global_step, ppl), args.global_rank)
+        if args.global_rank <= 0:
+            save_model(model, tokenizer, args.output_dir, f"epoch-{epoch + 1}-step-{global_step}")
+
 
 if __name__ == "__main__":
     train()
