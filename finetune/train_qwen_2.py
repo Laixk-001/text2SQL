589415125eec9e9a31bba3dafda22bce54e3369f
赖笑慷 <laixiaokang@4paradigm.com>
2024-11-05 00:25:13 +0800

    up

--- finetune/train_qwen_2.py
+++ finetune/train_qwen_2.py
@@ -9,6 +9,8 @@ from tqdm import tqdm
 import torch
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from torch.utils.data.distributed import DistributedSampler
+import torch.distributed as dist
+from transformers import Trainer
 import deepspeed
 from transformers import BitsAndBytesConfig
 from utils.utils import print_trainable_parameters, print_rank_0, to_device, set_random_seed, save_model,find_all_linear_names, evaluation, SupervisedDataset, DataCollatorForSupervisedDataset
@@ -58,6 +60,22 @@ def parse_args():
     parser = deepspeed.add_config_arguments(parser)
     return parser.parse_args()
 
+def is_master():
+    return dist.get_rank() == 0
+
+
+class LoggingCallback(transformers.TrainerCallback):
+    def on_log(self, args, state, control, logs=None, **kwargs):
+        if logs is not None:
+            log_message = {
+                "loss": logs.get("loss", None),
+                "learning_rate": logs.get("learning_rate", None),
+                "epoch": logs.get("epoch", None),
+                "step": state.global_step
+            }
+            if is_master():
+                print(log_message)
+
 def train():
     # 设置模型训练参数
     args = parse_args()
@@ -87,6 +105,7 @@ def train():
         padding_side = "right",
         trust_remote_code = True
     )
+    tokenizer.add_special_tokens({"additional_special_tokens": ["<|im_end|>", "<|im_start|>"]})
     # 加载qwen模型
     # device_map将模型映射到当前进程对应的GPU上
     device_map = {'': int(os.environ.get('LOCAL_RANK', '0'))}
@@ -139,6 +158,8 @@ def train():
     
     # DataCollator使用分词器进行数据的批处理，将单个样本打包成模型可接受的批次格式
     data_collator = DataCollatorForSupervisedDataset(tokenizer=tokenizer)
+    data_module = dict(train_dataset=train_dataset, eval_dataset=test_dataset, data_collator=data_collator)
+
     train_dataloader = DataLoader(train_dataset,
                                    collate_fn=data_collator,
                                    sampler=train_sampler,
@@ -155,25 +176,25 @@ def train():
     with open(args.ds_file, "r", encoding="utf-8")as f:
         ds_config = json.load(f)
         # 每个GPU的训练微批大小
-    ds_config["train_micro_batch_size_per_gpu"] = args.per_device_train_batch_size
-        # 总训练批大小 = 微批 * GPU数量 * 梯度累积步数
-    ds_config['train_batch_size'] = args.per_device_train_batch_size * torch.distributed.get_world_size() * args.gradient_accumulation_steps
-    ds_config["gradient_accumulation_steps"] = args.gradient_accumulation_steps
-    # load optimizer
-    ds_config["optimizer"]["params"]["lr"] = args.learning_rate
-        # Adam的动量参数
-    ds_config["optimizer"]["params"]["betas"] = (0.9, 0.95)
-    ds_config["optimizer"]["params"]["eps"] = 1e-8
-    ds_config["optimizer"]["params"]["weight_decay"] = 0.1    
-    num_training_steps = args.num_train_epochs * math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
-    print_rank_0("num_training_steps = {}".format(num_training_steps), args.global_rank)
-    num_warmup_steps = int(args.warmup_ratio * num_training_steps)
-    print_rank_0("num_warmup_steps = {}".format(num_warmup_steps), args.global_rank)
-    # 在预热阶段会逐步增加学习率，从而避免初始学习率过大导致的不稳定训练
-    ds_config["scheduler"]["params"]["total_num_steps"] = num_training_steps
-    ds_config["scheduler"]["params"]["warmup_num_steps"] = num_warmup_steps
-    ds_config["scheduler"]["params"]["warmup_max_lr"] = args.learning_rate
-    ds_config["scheduler"]["params"]["warmup_min_lr"] = args.learning_rate * 0.1
+    # ds_config["train_micro_batch_size_per_gpu"] = args.per_device_train_batch_size
+    #     # 总训练批大小 = 微批 * GPU数量 * 梯度累积步数
+    # ds_config['train_batch_size'] = args.per_device_train_batch_size * torch.distributed.get_world_size() * args.gradient_accumulation_steps
+    # ds_config["gradient_accumulation_steps"] = args.gradient_accumulation_steps
+    # # load optimizer
+    # ds_config["optimizer"]["params"]["lr"] = args.learning_rate
+    #     # Adam的动量参数
+    # ds_config["optimizer"]["params"]["betas"] = (0.9, 0.95)
+    # ds_config["optimizer"]["params"]["eps"] = 1e-8
+    # ds_config["optimizer"]["params"]["weight_decay"] = 0.1    
+    # num_training_steps = args.num_train_epochs * math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
+    # print_rank_0("num_training_steps = {}".format(num_training_steps), args.global_rank)
+    # num_warmup_steps = int(args.warmup_ratio * num_training_steps)
+    # print_rank_0("num_warmup_steps = {}".format(num_warmup_steps), args.global_rank)
+    # # 在预热阶段会逐步增加学习率，从而避免初始学习率过大导致的不稳定训练
+    # ds_config["scheduler"]["params"]["total_num_steps"] = num_training_steps
+    # ds_config["scheduler"]["params"]["warmup_num_steps"] = num_warmup_steps
+    # ds_config["scheduler"]["params"]["warmup_max_lr"] = args.learning_rate
+    # ds_config["scheduler"]["params"]["warmup_min_lr"] = args.learning_rate * 0.1
 
     # 设置模型gradient_checkpointing
     if args.gradient_checkpointing:
@@ -188,49 +209,55 @@ def train():
     
     # DeepSpeed对模型进行初始化
     model, optimizer, _ , lr_scheduler = deepspeed.initialize(model=model,args=args, config=ds_config, dist_init_required=True)
-    tr_loss, logging_loss, min_loss = 0.0, 0.0, 0.0
-    global_step = 0
-    # 模型开始训练
-    for epoch in range(args.num_train_epochs):
-        print_rank_0("Beginning of Epoch {}/{}, Total Micro Batches {}".format(epoch + 1, args.num_train_epochs, len(train_dataloader)), args.global_rank)
-        model.train()
-        # 遍历所有数据
-        for step, batch in tqdm(enumerate(train_dataloader), total=len(train_dataloader), unit="batch"):
-            batch = to_device(batch, device)
-            # 获取训练结果
-            outputs = model(**batch, use_cache=False)
-            loss = outputs.loss
-            # 损失函数反向传播，计算每个参数的梯度
-            model.backward(loss)
-            tr_loss += loss.item()
-            # 限制梯度的最大范数为1.0，避免梯度爆炸
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
-            # 更新模型参数
-            model.step()
-            # 当训练步数整除累积步数的时候，记录训练损失值和模型保存
-            if (step + 1) % args.gradient_accumulation_steps == 0:
-                global_step += 1
-                # 损失值记录
-                if global_step % args.show_loss_step == 0:
-                    if args.global_rank <= 0:
-                        tb_write.add_scalar("train_loss", (tr_loss - logging_loss) / (args.show_loss_step * args.gradient_accumulation_steps), global_step)
-                        logging_loss = tr_loss
-                # 模型保存并验证测试集的PPL值
-                if args.save_model_step is not None and global_step % args.save_model_step == 0:
-                    ppl = evaluation(model, test_dataloader, device)
-                    if args.global_rank <= 0:
-                        tb_write.add_scalar("ppl", ppl, global_step)
-                        print_rank_0("save_model_step-{}: ppl-{}".format(global_step, ppl), args.global_rank)
-                    if args.global_rank <= 0:
-                        save_model(model, tokenizer, args.output_dir, f"epoch-{epoch + 1}-step-{global_step}")
-                    model.train()
-        # 每个epoch对模型进行一次测试， 记录测试集损失
-        ppl = evaluation(model,test_dataloader,device)
-        if args.global_rank <= 0:
-            tb_write.add_scalar("ppl", ppl, global_step)
-            print_rank_0("save_model_step-{}: ppl-{}".format(global_step, ppl), args.global_rank)
-        if args.global_rank <= 0:
-            save_model(model, tokenizer, args.output_dir, f"epoch-{epoch + 1}-step-{global_step}")
+    trainer = Trainer(model=model, tokenizer=tokenizer, args=args, **data_module, callbacks=[LoggingCallback])
+    trainer.train()
+    trainer.save_state()
+    trainer.save_model(output_dir=args.output_dir)
+
+
+    # tr_loss, logging_loss, min_loss = 0.0, 0.0, 0.0
+    # global_step = 0
+    # # 模型开始训练
+    # for epoch in range(args.num_train_epochs):
+    #     print_rank_0("Beginning of Epoch {}/{}, Total Micro Batches {}".format(epoch + 1, args.num_train_epochs, len(train_dataloader)), args.global_rank)
+    #     model.train()
+    #     # 遍历所有数据
+    #     for step, batch in tqdm(enumerate(train_dataloader), total=len(train_dataloader), unit="batch"):
+    #         batch = to_device(batch, device)
+    #         # 获取训练结果
+    #         outputs = model(**batch, use_cache=False)
+    #         loss = outputs.loss
+    #         # 损失函数反向传播，计算每个参数的梯度
+    #         model.backward(loss)
+    #         tr_loss += loss.item()
+    #         # 限制梯度的最大范数为1.0，避免梯度爆炸
+    #         torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+    #         # 更新模型参数
+    #         model.step()
+    #         # 当训练步数整除累积步数的时候，记录训练损失值和模型保存
+    #         if (step + 1) % args.gradient_accumulation_steps == 0:
+    #             global_step += 1
+    #             # 损失值记录
+    #             if global_step % args.show_loss_step == 0:
+    #                 if args.global_rank <= 0:
+    #                     tb_write.add_scalar("train_loss", (tr_loss - logging_loss) / (args.show_loss_step * args.gradient_accumulation_steps), global_step)
+    #                     logging_loss = tr_loss
+    #             # 模型保存并验证测试集的PPL值
+    #             if args.save_model_step is not None and global_step % args.save_model_step == 0:
+    #                 ppl = evaluation(model, test_dataloader, device)
+    #                 if args.global_rank <= 0:
+    #                     tb_write.add_scalar("ppl", ppl, global_step)
+    #                     print_rank_0("save_model_step-{}: ppl-{}".format(global_step, ppl), args.global_rank)
+    #                 if args.global_rank <= 0:
+    #                     save_model(model, tokenizer, args.output_dir, f"epoch-{epoch + 1}-step-{global_step}")
+    #                 model.train()
+    #     # 每个epoch对模型进行一次测试， 记录测试集损失
+    #     ppl = evaluation(model,test_dataloader,device)
+    #     if args.global_rank <= 0:
+    #         tb_write.add_scalar("ppl", ppl, global_step)
+    #         print_rank_0("save_model_step-{}: ppl-{}".format(global_step, ppl), args.global_rank)
+    #     if args.global_rank <= 0:
+    #         save_model(model, tokenizer, args.output_dir, f"epoch-{epoch + 1}-step-{global_step}")
 
 if __name__ == "__main__":
     train()
