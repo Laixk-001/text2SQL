8a08c82730bf759f104683cf4dabada15419c40f
赖笑慷 <laixiaokang@4paradigm.com>
2024-11-04 13:55:30 +0800

    up

---
+++ data/scripts/binarize_data.sh
@@ -0,0 +1,9 @@
+# export PATH=/path/to/miniconda3/envs/qwen/bin:$PATH;
+# cd ./Qwen2.5-Coder-evaluation/sft/;
+INPUT_PATH=${1}
+OUTPUT_PATH=${2}
+TOKENIZER_PATH=${3}
+INPUT_PATH=${INPUT_PATH:-"/root/autodl-fs/DuSQL/text2sql_dev_text_zh.json"}
+OUTPUT_PATH=${OUTPUT_PATH:-"/root/autodl-fs/DuSQL/text2sql_dev_tokenizer_zh.json"}
+TOKENIZER_PATH=${TOKENIZER_PATH:-"/root/autodl-fs/Qwen2_5_Coder_7B_Instruct/"}
+python binarize_data.py -input_path ${INPUT_PATH} -output_path ${OUTPUT_PATH} -workers 64 -tokenizer_path ${TOKENIZER_PATH}
