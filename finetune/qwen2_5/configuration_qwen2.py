13a91f5489bb8f74b4dd3776d2b6f221ccada00c
赖笑慷 <laixiaokang@4paradigm.com>
2024-11-05 14:28:26 +0800

    up

--- finetune/qwen2_5/configuration_qwen2.py
+++ finetune/qwen2_5/configuration_qwen2.py
@@ -181,7 +181,7 @@ class Qwen2Config(PretrainedConfig):
         # BC: if there is a 'type' field, move it to 'rope_type'.
         if self.rope_scaling is not None and "type" in self.rope_scaling:
             self.rope_scaling["rope_type"] = self.rope_scaling["type"]
-        rope_config_validation(self)
+        # rope_config_validation(self)
 
         super().__init__(
             tie_word_embeddings=tie_word_embeddings,
