831aa7602f9a5588ad95ca8bb5f6cdab883d4907
赖笑慷 <laixiaokang@4paradigm.com>
2024-11-05 23:22:21 +0800

    up

--- finetune/qwen2_5/modeling_qwen2.py
+++ finetune/qwen2_5/modeling_qwen2.py
@@ -1197,7 +1197,7 @@ class Qwen2ForCausalLM(Qwen2PreTrainedModel, GenerationMixin):
             shift_logits = logits[..., :-1, :].contiguous()
             shift_labels = labels[..., 1:].contiguous()
             # Flatten the tokens
-            loss_fct = CrossEntropyLoss()
+            loss_fct = CrossEntropyLoss(ignore_index=-1)
             shift_logits = shift_logits.view(-1, self.config.vocab_size)
             shift_labels = shift_labels.view(-1)
             # Enable model parallelism
